0663aeefece075c33bfff79cc2e082a5ca87b8de
AyoubDjaghloul <ayoubdj19@gmail.com>
2022-12-02 14:22:21 +0100

    interface comp1 (combobox)

--- actions/v0_action_fct_comp_1_partie_1.py
+++ actions/v0_action_fct_comp_1_partie_1.py
@@ -19,20 +19,17 @@ class AppFctComp1Partie1(QDialog):
     def refreshResult(self):
         # TODO 1.1 : fonction à modifier pour remplacer la zone de saisie par une liste de valeurs prédéfinies
         #  dans l'interface une fois le fichier ui correspondant mis à jour
-        display.refreshLabel(self.ui.label_fct_comp_1, "")
-        if not self.ui.lineEdit_fct_comp_1.text().strip():
+        try:
+            cursor = self.data.cursor()
+            result = cursor.execute(
+                "SELECT numEp, nomEp, formeEp, nomDi, categorieEp, nbSportifsEp, strftime('%Y-%m-%d',dateEp,'unixepoch') FROM V0_LesEpreuves WHERE categorieEp = ?",
+                [self.ui.comboBox_11.currentText().strip()])
+        except Exception as e:
             self.ui.table_fct_comp_1.setRowCount(0)
-            display.refreshLabel(self.ui.label_fct_comp_1, "Veuillez indiquer un nom de catégorie")
+            display.refreshLabel(self.ui.label_fct_comp_1, "Impossible d'afficher les résultats : " + repr(e))
         else:
-            try:
-                cursor = self.data.cursor()
-                result = cursor.execute(
-                    "SELECT numEp, nomEp, formeEp, nomDi, categorieEp, nbSportifsEp, strftime('%Y-%m-%d',dateEp,'unixepoch') FROM V0_LesEpreuves WHERE categorieEp = ?",
-                    [self.ui.lineEdit_fct_comp_1.text().strip()])
-            except Exception as e:
-                self.ui.table_fct_comp_1.setRowCount(0)
-                display.refreshLabel(self.ui.label_fct_comp_1, "Impossible d'afficher les résultats : " + repr(e))
+            i = display.refreshGenericData(self.ui.table_fct_comp_1, result)
+            if i == 0:
+                display.refreshLabel(self.ui.label_fct_comp_1, "Aucun résultat")
             else:
-                i = display.refreshGenericData(self.ui.table_fct_comp_1, result)
-                if i == 0:
-                    display.refreshLabel(self.ui.label_fct_comp_1, "Aucun résultat")
+                display.refreshLabel(self.ui.label_fct_comp_1, "")
