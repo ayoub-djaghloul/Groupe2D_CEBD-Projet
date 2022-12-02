52de852738b474285ffc19b485003afb2ae65ddb
AyoubDjaghloul <ayoubdj19@gmail.com>
2022-12-02 16:17:33 +0100

    interface comp1 (combobox)

--- data/v1_deleteDB.sql
+++ data/v1_deleteDB.sql
@@ -1,4 +1,12 @@
 -- TODO 1.3a : Détruire les tables manquantes et modifier celles ci-dessous
-DROP TABLE IF EXISTS LesEpreuves;
-DROP TABLE IF EXISTS LesSportifsEQ;
+DROP VIEW IF EXISTS AgesSportifs;
+DROP VIEW IF EXISTS LesNbsEquipiers;
+DROP TABLE IF EXISTS Resultat;
+DROP TABLE IF EXISTS Epreuve;
+DROP TABLE IF EXISTS Inscription;
+DROP TABLE IF EXISTS Sportif_base;
+DROP TABLE IF EXISTS Discipline;
+DROP TABLE IF EXISTS Equipe_base;
+DROP TABLE IF EXISTS EstEquipier;
+DROP TABLE IF EXISTS Participant;
 -- TODO 3.3 : pensez à détruire vos triggers !
