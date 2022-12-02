21fbebb846de34dd8948f5765e964727c45d6731
AyoubDjaghloul <ayoubdj19@gmail.com>
2022-12-02 16:09:18 +0100

    interface comp1 (combobox)

--- actions/v0_action_fct_comp_2_partie_1.py
+++ actions/v0_action_fct_comp_2_partie_1.py
@@ -9,29 +9,40 @@ from PyQt5 import uic
 class AppFctComp2Partie1(QDialog):
 
     # Constructeur
-    def __init__(self, data:sqlite3.Connection):
+    def __init__(self, data: sqlite3.Connection):
         super(QDialog, self).__init__()
         self.ui = uic.loadUi("gui/fct_comp_2.ui", self)
         self.data = data
+        try:
+            cursor = self.data.cursor()
+            result = cursor.execute(
+                "SELECT DISTINCT(categorieEp) FROM V0_LesEpreuves"
+            )
+        except Exception as e:
+            self.ui.table_fct_comp_3.setRowCount(0)
+            display.refreshLabel(self.ui.label_fct_comp_2, "Impossible d'afficher les résultats : " + repr(e))
+        else:
+            self.ui.combBox_12.clear()
+            for categorie in result.fetchall():
+                self.ui.combBox_12.addItem(str(categorie[0]))
 
     # Fonction de mise à jour de l'affichage
+    @pyqtSlot()
     def refreshResult(self):
         # TODO 1.2 : fonction à modifier pour remplacer la zone de saisie par une liste de valeurs issues de la BD une
         #  fois le fichier ui correspondant mis à jour
-        display.refreshLabel(self.ui.label_fct_comp_2, "")
-        if not self.ui.lineEdit_fct_comp_2.text().strip():
+        try:
+            cursor = self.data.cursor()
+            result = cursor.execute(
+                "SELECT numEp, nomEp, formeEp, nomDi, categorieEp, nbSportifsEp, strftime('%Y-%m-%d',dateEp,'unixepoch') FROM V0_LesEpreuves WHERE categorieEp = ?",
+                [self.ui.combBox_12.currentText().strip()])
+        except Exception as e:
             self.ui.table_fct_comp_2.setRowCount(0)
-            display.refreshLabel(self.ui.label_fct_comp_2, "Veuillez indiquer un nom de catégorie")
+            display.refreshLabel(self.ui.label_fct_comp_2, "Impossible d'afficher les résultats : " + repr(e))
         else:
-            try:
-                cursor = self.data.cursor()
-                result = cursor.execute(
-                    "SELECT numEp, nomEp, formeEp, nomDi, categorieEp, nbSportifsEp, strftime('%Y-%m-%d',dateEp,'unixepoch') FROM LesEpreuves WHERE categorieEp = ?",
-                    [self.ui.lineEdit_fct_comp_2.text().strip()])
-            except Exception as e:
-                self.ui.table_fct_comp_3.setRowCount(0)
-                display.refreshLabel(self.ui.label_fct_comp_2, "Impossible d'afficher les résultats : " + repr(e))
+            i = display.refreshGenericData(self.ui.table_fct_comp_2, result)
+            if i == 0:
+                print("lol")
+                display.refreshLabel(self.ui.label_fct_comp_2, "Aucun résultat")
             else:
-                i = display.refreshGenericData(self.ui.table_fct_comp_2, result)
-                if i == 0:
-                    display.refreshLabel(self.ui.label_fct_comp_2, "Aucun résultat")
+                display.refreshLabel(self.ui.label_fct_comp_2, "")
--- data/v1_createDB.sql
+++ data/v1_createDB.sql
@@ -1,34 +1,100 @@
 -- TODO 1.3a : Créer les tables manquantes et modifier celles ci-dessous
-CREATE TABLE LesSportifsEQ
+
+CREATE TABLE Sportif_base
 (
-  numSp NUMBER(4),
-  nomSp VARCHAR2(20),
-  prenomSp VARCHAR2(20),
-  pays VARCHAR2(20),
-  categorieSp VARCHAR2(10),
-  dateNaisSp DATE,
-  numEq NUMBER(4),
-  CONSTRAINT SP_CK1 CHECK(numSp > 0),
-  CONSTRAINT SP_CK2 CHECK(categorieSp IN ('feminin','masculin')),
-  CONSTRAINT SP_CK3 CHECK(numEq > 0)
+    numSp INTEGER PRIMARY KEY,
+    nomSp VARCHAR(20) NOT NULL,
+    prenomSp VARCHAR(20) NOT NULL,
+    pays VARCHAR(20),
+    categorieSp VARCHAR(10),
+    dateNaisSp DATE,
+
+    CONSTRAINT SP_CK1 CHECK(numSp > 0),
+    CONSTRAINT SP_CK2 CHECK(categorieSp IN ('feminin','masculin')),
+
+    FOREIGN KEY (numSp) REFERENCES Participant(numPart) ON DELETE CASCADE
 );
 
-CREATE TABLE LesEpreuves
+CREATE TABLE Epreuve
 (
-  numEp NUMBER(3),
-  nomEp VARCHAR2(20),
-  formeEp VARCHAR2(13),
-  nomDi VARCHAR2(25),
-  categorieEp VARCHAR2(10),
-  nbSportifsEp NUMBER(2),
-  dateEp DATE,
-  CONSTRAINT EP_PK PRIMARY KEY (numEp),
-  CONSTRAINT EP_CK1 CHECK (formeEp IN ('individuelle','par equipe','par couple')),
-  CONSTRAINT EP_CK2 CHECK (categorieEp IN ('feminin','masculin','mixte')),
-  CONSTRAINT EP_CK3 CHECK (numEp > 0),
-  CONSTRAINT EP_CK4 CHECK (nbSportifsEp > 0)
+    numEp INTEGER PRIMARY KEY,
+    nomEp VARCHAR(30),
+    formeEp VARCHAR(15),
+    nomDi VARCHAR(50),
+    categorieEp VARCHAR(10),
+    nbSportifsEp INTEGER,
+    dateEp DATE,
+
+    CONSTRAINT EP_CK1 CHECK (formeEp IN ('individuelle','par equipe','par couple')),
+    CONSTRAINT EP_CK2 CHECK (categorieEp IN ('feminin','masculin','mixte')),
+    CONSTRAINT EP_CK3 CHECK (numEp > 0),
+    CONSTRAINT EP_CK4 CHECK (nbSportifsEp > 0),
+
+    FOREIGN KEY (nomDi) REFERENCES Discipline(nomDi) ON DELETE CASCADE
+);
+
+CREATE TABLE Resultat
+(
+    numEp INTEGER PRIMARY KEY,
+    gold INTEGER,
+    silver INTEGER,
+    bronze INTEGER,
+
+    FOREIGN KEY (numEp) REFERENCES Epreuve(numEp) ON DELETE CASCADE,
+    FOREIGN KEY (gold) REFERENCES Participant(numPart),
+    FOREIGN KEY (silver) REFERENCES Participant(numPart),
+    FOREIGN KEY (bronze) REFERENCES Participant(numPart)
+);
+
+CREATE TABLE Participant
+(
+    numPart INTEGER PRIMARY KEY,
+
+    CONSTRAINT PA_CK1 CHECK ((numPart > 0 and numPart < 100) or (numPart > 1000 and numPart < 1500))
+);
+
+CREATE TABLE Equipe_base
+(
+    numEq INTEGER PRIMARY KEY,
+
+    FOREIGN KEY (numEq) REFERENCES Participant(numPart) ON DELETE CASCADE
+);
+
+CREATE TABLE EstEquipier
+(
+    numEq INTEGER,
+    numSp INTEGER,
+
+    CONSTRAINT ESEQ_PK PRIMARY KEY (numEq, numSp),
+
+    FOREIGN KEY (numEq) REFERENCES Equipe_base(numEq) ON DELETE CASCADE,
+    FOREIGN KEY (numSp) REFERENCES Sportif_base(numSp) ON DELETE CASCADE
+);
+
+CREATE TABLE Inscription
+(
+    numPart INTEGER,
+    numEp INTEGER,
+
+    CONSTRAINT INS_PK PRIMARY KEY (numPart, numEp),
+
+    FOREIGN KEY (numPart) REFERENCES Participant(numPart) ON DELETE CASCADE,
+    FOREIGN KEY (numEp) REFERENCES Epreuve(numEp) ON DELETE CASCADE
+);
+
+CREATE TABLE Discipline
+(
+    nomDi VARCHAR(50) PRIMARY KEY
 );
 
 -- TODO 1.4a : ajouter la définition de la vue LesAgesSportifs
+CREATE VIEW AgesSportifs(numSp, nomSp, prenomSp, pays, categorieSp, dateNaisSp, age) AS
+    SELECT numSp, nomSp, prenomSp, pays, categorieSp, dateNaisSp, CURRENT_DATE - dateNaisSp AS age
+    FROM Sportif_base;
+
 -- TODO 1.5a : ajouter la définition de la vue LesNbsEquipiers
+CREATE VIEW LesNbsEquipiers(numEq, nbEquipiers) AS
+    SELECT numEq, COUNT(numSp) AS nbEquipiers
+    FROM Equipe_base JOIN EstEquipier USING(numEq)
+    GROUP BY numEq;
 -- TODO 3.3 : ajouter les éléments nécessaires pour créer le trigger (attention, syntaxe SQLite différent qu'Oracle)
